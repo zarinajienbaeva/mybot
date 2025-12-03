e7730248957438052111e6d8d6467da562aac61d
zarinajienbaeva <zarinajienbaeva06@gmail.com>
2025-12-03 14:04:36 +0500

    add photo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher , Z
+from aiogram import types
 from aiogram.filters import CommandStart
 from config import token
 import asyncio
@@ -8,13 +9,25 @@ dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def handle_start(message):
-    await message.answer("hi")
+async def handle_start(message: types.Message):
+    await message.answer(f"Welcome {message.from_user.full_name} ")
+
+@dp.message(CommandStart("info"))
+async def handle_info(message: types.Message):
+    await message.answer(f"this text {message.from_user.full_name} ")
+
+
+
+@dp.message(Z.photo)
+async def handle_photo(message: types.Message):
+    await message.copy_to(chat_id=message.chat.id)
+
 
 
 async def main():
     print("I am starting ...")
     await dp.start_polling(my_bot)
+
 
 
 if __name__ == "__main__":
